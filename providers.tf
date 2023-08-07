f35df479245bdb151b634a648c2ea9dee32fc9f4
dawsonpaul <dawsonpaul@gmail.com>
2023-08-07 19:37:11 +0000

    Initial Commit

--- providers.tf
+++ providers.tf
@@ -10,8 +10,8 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  client_id       = "${env.AZURE_CLIENT_ID}"
-  client_secret   = "${env.AZURE_CLIENT_SECRET}"
-  tenant_id       = "${env.AZURE_TENANT_ID}"
-  subscription_id = "${env.AZURE_SUBSCRIPTION_ID}"
+  client_id       = env.AZURE_CLIENT_ID
+  client_secret   = env.AZURE_CLIENT_SECRET
+  tenant_id       = env.AZURE_TENANT_ID
+  subscription_id = env.AZURE_SUBSCRIPTION_ID
 }
