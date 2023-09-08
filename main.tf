46ad4f0a9f188575809b6ea7f49118d23221d48d
dawsonpaul <dawsonpaul@gmail.com>
2023-09-08 10:23:58 +0000

    Prevent

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "waflab" {
   location = "West Europe"
 }
 
-# Create a virtual network - 
+# Create a virtual network --
 resource "azurerm_virtual_network" "waflab" {
   name                = "waflab-network"
   resource_group_name = azurerm_resource_group.waflab.name
